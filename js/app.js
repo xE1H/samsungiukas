daa30eb0e7a1ad7245ae8a6ebe9b662c8c62fda1
xE1H <me@nojus.dev>
2023-12-12 13:41:06 +0200

    snap scroll & other fixes

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ function initComparisons() {
     pass the "overlay" element as a parameter when executing the compareImages function: */
     compareImages(x[i]);
   }
+
   function compareImages(img) {
     var slider, img, clicked = 0, w, h;
     /* Get the width and height of the img element */
@@ -30,6 +31,7 @@ function initComparisons() {
     slider.addEventListener("touchstart", slideReady);
     /* And released (for touch screens: */
     window.addEventListener("touchend", slideFinish);
+
     function slideReady(e) {
       /* Prevent any other actions that may occur when moving over the image: */
       e.preventDefault();
@@ -39,10 +41,12 @@ function initComparisons() {
       window.addEventListener("mousemove", slideMove);
       window.addEventListener("touchmove", slideMove);
     }
+
     function slideFinish() {
       /* The slider is no longer clicked: */
       clicked = 0;
     }
+
     function slideMove(e) {
       var pos;
       /* If the slider is no longer clicked, exit this function: */
@@ -55,6 +59,7 @@ function initComparisons() {
       /* Execute a function that will resize the overlay image according to the cursor: */
       slide(pos);
     }
+
     function getCursorPos(e) {
       var a, x = 0;
       e = (e.changedTouches) ? e.changedTouches[0] : e;
@@ -66,6 +71,7 @@ function initComparisons() {
       x = x - window.pageXOffset;
       return x;
     }
+
     function slide(x) {
       /* Resize the image: */
       img.style.width = x + "px";
@@ -73,4 +79,75 @@ function initComparisons() {
       slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
     }
   }
+}
+
+
+function smoothScroll() {
+  const divs = Array.from(document.querySelectorAll('body > div'));
+  let isScrolling = false;
+
+  let lastScrollTop = 0;
+
+  function getClosestDiv(deltaY) {
+    let closest = null;
+    let minDistance = Number.MAX_VALUE;
+    let scrollDirection = null;
+
+    // Determine the scroll direction
+    if (deltaY < 0) {
+      scrollDirection = 'down';
+    } else {
+      scrollDirection = 'up';
+    }
+
+    divs.forEach((div) => {
+      const rect = div.getBoundingClientRect();
+      const distance = Math.abs(rect.top - window.innerHeight / 2);
+
+      if (distance < minDistance) {
+        // If scrolling down, select the div below the current one
+        // If scrolling up, select the div above the current one
+        if ((scrollDirection === 'down' && rect.top > 0) || (scrollDirection === 'up' && rect.bottom < window.innerHeight)) {
+          minDistance = distance;
+          closest = div;
+        }
+      }
+    });
+
+    return closest;
+  }
+
+  function scrollToElement(element) {
+    //isScrolling = true;
+    element.scrollIntoView({behavior: 'smooth'});
+  }
+
+  function handleScroll(e) {
+    e.preventDefault(); // Prevent default scrolling
+
+    if (isScrolling) return;
+
+    let deltaY;
+    let scrollDirection;
+
+    if (e.type === 'wheel') {
+      deltaY = e.deltaY;
+    } else if (e.type === 'touchmove') {
+      deltaY = e.touches[0].clientY;
+      deltaY = deltaY < window.innerHeight / 2 ? 1 : -1;
+    }
+
+    const closestDiv = getClosestDiv(deltaY);
+
+    if (closestDiv) {
+      scrollToElement(closestDiv);
+    }
+  }
+
+  window.addEventListener('wheel', handleScroll, {passive: false}); // Declare the listener as non-passive
+  window.addEventListener('touchmove', handleScroll, {passive: false}); // Declare the listener as non-passive for touch devices
+
+  window.addEventListener('scrollend', () => {
+    isScrolling = false;
+  });
 }
