21d80e8ae00a2cfe17b692e7247c11fdb6e9d939
nojus <me@nojus.dev>
2023-12-12 19:32:26 +0200

    anim

--- js/app.js
+++ js/app.js
@@ -87,18 +87,18 @@ function initComparisons() {
 }
 
 
-const lenis = new Lenis();
-
-lenis.on('scroll', (e) => {
-  //console.log(e);
-})
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
+// const lenis = new Lenis();
+//
+// lenis.on('scroll', (e) => {
+//   //console.log(e);
+// })
+//
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+//
+// requestAnimationFrame(raf);
 
 function initAnimations() {
   var slider = document.getElementsByClassName("img-comp-slider")[0];
@@ -136,4 +136,69 @@ function initAnimations() {
   ).to({}, {'duration': 1}).set("body", {
     'overflow-y': 'hidden !important'
   });
+
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: '#tinklalaides',
+      start: 'top',
+      end: 'bottom',
+      scrub: 1,
+      pin: "#tinklalaides"
+    }
+  }).to({}, {'duration': 1}).set("body", {
+    'overflow-y': 'hidden !important'
+  });
+}
+
+let scene, camera, renderer, model;
+
+function init3dModel() {
+  // Create the scene and set the scene size.
+  scene = new THREE.Scene();
+  const WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight;
+
+  // Create a renderer and add it to the DOM.
+  renderer = new THREE.WebGLRenderer({antialias:true});
+  renderer.setSize(WIDTH, HEIGHT);
+  document.body.appendChild(renderer.domElement);
+
+  // Create a camera, zoom it out from the model a bit, and add it to the scene.
+  camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
+  camera.position.set(0,2,10);
+  scene.add(camera);
+
+  // Create a light and add it to the scene.
+  const light = new THREE.HemisphereLight(0xffffff, 0x444444);
+  light.position.set(0, 20, 0);
+  scene.add(light);
+
+  // Load the GLTF model using GLTFLoader
+  const loader = new THREE.GLTFLoader();
+  loader.load('path_to_your_model.glb', function(gltf) {
+    model = gltf.scene;
+    scene.add(model);
+  }, undefined, function(error) {
+    console.error(error);
+  });
+
+  // Resize Update
+  window.addEventListener('resize', () => {
+    const WIDTH = window.innerWidth,
+      HEIGHT = window.innerHeight;
+    renderer.setSize(WIDTH, HEIGHT);
+    camera.aspect = WIDTH / HEIGHT;
+    camera.updateProjectionMatrix();
+  });
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Rotate the model over time
+  if(model) {
+    model.rotation.y += 0.005;
+  }
+
+  renderer.render(scene, camera);
 }
